5df643ee5b8436bd499959c1136fd865ba2f82d5
dhyan-patel2008 <86015221+dhyan-patel2008@users.noreply.github.com>
2021-06-20 15:36:30 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:/Users/comp/Desktop/FIB.py ====================
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+>>> 
