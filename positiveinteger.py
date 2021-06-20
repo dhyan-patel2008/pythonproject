1953f5b100c2081c037307129e47d64f33461332
dhyan-patel2008 <86015221+dhyan-patel2008@users.noreply.github.com>
2021-06-20 15:55:46 +0530

    Add files via upload

---
+++ positiveinteger.py
@@ -0,0 +1,9 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/comp/AppData/Local/Programs/Python/Python38/int.py =====
+original numbers in the list:  [12, -7, 5, 64, -14]
+positive number in the list:  [12, 5, 64]
+original numbers in the list:  [12, 14, -95, 3]
+positive number in the list:  [12, 14, 3]
+>>> 
