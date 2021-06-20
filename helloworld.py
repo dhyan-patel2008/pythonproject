64079582fe98f2cfa008cb52a97ee3bd184518e0
dhyan-patel2008 <86015221+dhyan-patel2008@users.noreply.github.com>
2021-06-20 15:07:13 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/comp/AppData/Local/Programs/Python/Python38/world.py ====
+Hello world
+>>> 
